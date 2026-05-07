8b7a4c520350bfcfcd5d871b82747d63af754f95
razzak <khalfallah.razzak@gmail.com>
2026-05-08 00:00:51 +0200

    ask-38 : Ajouter la doc API pour chaque endpoint

--- src/main/java/com/bookNDrive/notification_service/controllers/MailController.java
+++ src/main/java/com/bookNDrive/notification_service/controllers/MailController.java
@@ -2,25 +2,47 @@ package com.bookNDrive.notification_service.controllers;
 
 import com.bookNDrive.notification_service.dtos.received.ForgotPassword;
 import com.bookNDrive.notification_service.services.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mails")
+@Tag(name = "Mail Controller", description = "Expose les operations d'envoi des emails de notification.")
 public class MailController {
 
     private final MailService mailService;
 
     @Autowired
-    public MailController(MailService mailService){
+    public MailController(MailService mailService) {
         this.mailService = mailService;
     }
 
+    @Operation(
+            summary = "Envoyer un email de reinitialisation",
+            description = "Declenche l'envoi d'un email de reinitialisation de mot de passe a partir de l'adresse email et du token fournis."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "202",
+                    description = "Demande d'envoi acceptee"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Corps de requete invalide"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Erreur technique pendant l'envoi du mail"
+            )
+    })
     @PostMapping("/forgot-password")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void sendMailToResetPassword(@RequestBody ForgotPassword forgotPassword){
+    public void sendMailToResetPassword(@RequestBody ForgotPassword forgotPassword) {
         mailService.sendForgotPasswordMail(forgotPassword);
     }
 }
